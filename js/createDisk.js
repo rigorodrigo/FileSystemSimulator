82ef3ae9be7208128baf3fff1f91021a55429040
Rodrigo Rigo <rodrigo.rigo@outlook.com.br>
2025-08-28 10:34:43 -0300

    criando o disco em si,sem nenhuma interatividade com usuário

---
+++ js/createDisk.js
@@ -0,0 +1,35 @@
+export default function createDisk(totalBlocks, blockSize) {
+
+    const container = document.getElementById('disk-blocks');
+    const blocks = [];
+
+    for (let i = 0 ; i < totalBlocks; i++) {
+        const tooltipWrapper = document.createElement('div');
+            tooltipWrapper.className = 'tooltip';
+                
+            const tooltipContent = document.createElement('div');
+            tooltipContent.className = 'tooltip-content';
+            tooltipContent.innerHTML = `
+                <div class="text-center">
+                    <div class="font-bold text-green-400">Bloco ${i}</div>
+                    <div class="text-xs text-gray-300">Status: Livre</div>
+                    <div class="text-xs text-gray-300">Tamanho: ${blockSize}</div>
+                </div>
+                `;
+                
+                const block = document.createElement('span');
+                block.className = 'w-4 h-4 bg-success inline-block rounded-sm border border-gray-200 cursor-pointer';
+                block.dataset.blockId = i;
+                block.dataset.status = 'free';
+
+                tooltipWrapper.appendChild(tooltipContent);
+                tooltipWrapper.appendChild(block);
+                container.appendChild(tooltipWrapper);
+
+                blocks.push(block);
+
+    }
+
+    return {blocks, totalBlocks,blockSize};
+
+}
---
+++ js/index.js
@@ -0,0 +1,5 @@
+import  createDisk  from './createDisk.js'
+
+  document.addEventListener('DOMContentLoaded', () => {
+            createDisk(2048,'4KB')
+        });
